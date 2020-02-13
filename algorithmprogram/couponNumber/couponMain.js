057ead0d5e781e11ac4fb88057bdb5ebf5fd6980
Dilip138 <dilipsoni199566@gmail.com>
2020-02-13 13:06:45 +0530

    [Ashwini] refactor main method of coupon number

--- algorithmprogram/couponNumber/couponMain.js
+++ algorithmprogram/couponNumber/couponMain.js
@@ -1,7 +1,20 @@
 let readline = require('readline-sync');
 let callFunction = require('./couponBL');
-try {
 
+    try {
+    let NoOfCoupon = readline.questionInt("how many times you want to generate coupon");
+    console.log("Enter" + NoOfCoupon + "distinct coupon between 10 to 100");
+    let coupon = [];
+    for (let i = 0; i < NoOfCoupon;){
+            let num = readline.questionInt();
+            if(num >= 1){
+                coupon.push(num);
+                i++;
+            } 
+        }
+        
+       let num= callFunction.CouponNumber(coupon); // calling CouponNumber()
+        console.log(num);
 } catch (error) {
-    
+    console.log(error);
 }
