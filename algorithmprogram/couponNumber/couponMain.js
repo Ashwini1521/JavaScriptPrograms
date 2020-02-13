761dcd82d789fa83b9d4e65151d5fcb6bfd26a77
Dilip138 <dilipsoni199566@gmail.com>
2020-02-13 13:15:16 +0530

    [Ashwini] refactor commented  main method of coupon number

--- algorithmprogram/couponNumber/couponMain.js
+++ algorithmprogram/couponNumber/couponMain.js
@@ -1,3 +1,19 @@
+
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node couponMain.js
+	 *                      2. if nodemon installed cmd> nodemon couponMain.js
+	 *
+	 *  Purpose         : CouponNumber.
+	 *
+	 *  @description    : taking input from given distinct coupon number and finding how many times coupon number is generating using random function...
+	 *
+	 *  @file           : couponMain.js
+	 *  @overview       : coupon number generation. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 13-02-2020
+	 ******************************************************************************/
 let readline = require('readline-sync');
 let callFunction = require('./couponBL');
 
