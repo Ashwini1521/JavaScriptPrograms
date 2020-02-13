78a36553b9871a0239c3a2b9de90ab0c51b55069
Dilip138 <dilipsoni199566@gmail.com>
2020-02-13 11:47:15 +0530

    [Ashwini] refactor main method

--- algorithmprogram/BinarySearch/binaryMain.js
+++ algorithmprogram/BinarySearch/binaryMain.js
@@ -2,12 +2,13 @@ let readline = require('readline-sync');
 let callFunction = require('./binaryBL');
 try {
     let array = [];
-    let arraysize = readline.question("Enter size of an array");
+    let arraysize = readline.question("Enter size of an array : ");
     for (var i=0; i<arraysize; i++){
-        array[i] readline.question("array element is: ");
+        array[i] = readline.question("array element is: ");
     }
-    let String = readline.question("Enter String to check in array");
-    let binary = callFunction.Binary(array,String);
+    let String = readline.question("Enter String to check in array : ");
+    let Binary = callFunction.Binary(array,String);
+    console.log(Binary);
 } catch (error) {
     console.log(error);
 }
