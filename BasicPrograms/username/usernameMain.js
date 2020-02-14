786d7483ca003bb1fab2c4e9515627c6d46cc078
Ashwini1521 <59740588+Ashwini1521@users.noreply.github.com>
2020-02-14 11:31:48 +0530

    delete usernameMain.js

--- BasicPrograms/username/usernameMain.js
+++
@@ -1,8 +0,0 @@
-const readline = require('readline-sync');
-const callfunction = require('./usernameBL');
-try {
-    let username = readline.question("Enter your name : ");
-    let result = callfunction.checkusername(username);
-} catch (error) {
-    console.log(error);
-}
