37e52fc3f08c953e810b47dae830c47b0e8689fb
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 14:56:51 +0530

    [Ashwini] refactor: indented business logic of harmonic number program

--- BasicPrograms/HarmonicNumber/harmonicBL.js
+++ BasicPrograms/HarmonicNumber/harmonicBL.js
@@ -1,10 +1,10 @@
-module.exports={
-    checkharmonic:(num) => {
+module.exports = {
+    checkharmonic: (num) => {
         console.log('Harmonic Number');
         console.log(num);
         let harmonic = 0.0;
-        for (let i=1; i<= num; i++){
-            harmonic = harmonic + (1+i);
+        for (let i = 1; i <= num; i++) {
+            harmonic = harmonic + (1 + i);
         }
         return harmonic;
     }
