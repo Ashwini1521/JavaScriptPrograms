d4ab9587868d215c9d297cfd0e99f5ffb2223399
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 18:30:13 +0530

    [Ashwini] refactor:indented business logic of balance paranthesis program

--- DataStructure/BalParBL.js
+++ DataStructure/BalParBL.js
@@ -1,37 +1,37 @@
-var size=100;
+var size = 100;
 
-    class stack {
+class stack {
     constructor() {
         this.a = [];
-        this.top=-1;
+        this.top = -1;
 
     }
     isEmpty() {
         if (this.top == -1) {
             return true;
         }
-        else{
+        else {
             return false;
         }
     }
     isFull() {
-        if (this.a.length-1 == this.top) {
+        if (this.a.length - 1 == this.top) {
             return true;
         }
-        else{
+        else {
             return false;
         }
     }
     push(element) {
-       this.top++;
-          return this.a.push(element);
+        this.top++;
+        return this.a.push(element);
     }
     pop() {
-        
+
         if (!this.isEmpty()) {
             this.top--;
-          return this.a.pop();
-        
+            return this.a.pop();
+
         }
         else {
             console.log("underflow");
@@ -40,38 +40,38 @@ var size=100;
     peek() {
         if (!this.isEmpty()) {
             return this.a[this.a.length - 1];
-            
+
         }
-        else{
+        else {
             console.log("is empty");
         }
     }
-   
+
 }
-   module.exports={
-        isBalanced:(expression)=>{
-            const s=new stack();
-            
-        for(let i=0;i<expression.length;i++){
-            if(expression[i]==='('){
+module.exports = {
+    isBalanced: (expression) => {
+        const s = new stack();
+
+        for (let i = 0; i < expression.length; i++) {
+            if (expression[i] === '(') {
                 s.push(expression[i]);
                 console.log(s);
 
             }
-            else if(expression[i]===')'){
-             
-             if(s.peek()==='('){
-                 s.pop();
-             }        
-            }
-        }
-            if(s.isEmpty()){
-                return true;
-            }
-            else{
-                return false;
-            }
+            else if (expression[i] === ')') {
 
+                if (s.peek() === '(') {
+                    s.pop();
+                }
+            }
         }
+        if (s.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
+}
 
