27186fa61b67406376228ebb5d43194d39c3e53d
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 14:58:21 +0530

    [Ashwini] refactor: indented main method of leapyear programk

--- BasicPrograms/leapyear/leapMain.js
+++ BasicPrograms/leapyear/leapMain.js
@@ -16,9 +16,9 @@
 const readline = require('readline-sync');
 const callfunction = require('./leapBL');
 try {
-    let year = readline.questionInt("enter 4 digit year : ");
-    callfunction.leapyear(year);
-    // return result;
+	let year = readline.questionInt("enter 4 digit year : ");
+	callfunction.leapyear(year);
+	// return result;
 } catch (error) {
-    console.log(error);
+	console.log(error);
 }
