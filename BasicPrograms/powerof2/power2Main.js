b8ca50693cd1789710337bd5d513dd028fc9258b
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 10:00:27 +0530

    [Ashwini] refactor:commentedon Main method of leap year program

--- BasicPrograms/powerof2/power2Main.js
+++ BasicPrograms/powerof2/power2Main.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node power2Main.js
+	 *                      2. if nodemon installed cmd> nodemon power2Main.js
+	 *
+	 *  Purpose         : to find power of 2. 
+	 *
+	 *  @description    : prints a table of the powers of 2 that are less than or equal to 2^N....
+	 *
+	 *  @file           : power2Main.js
+	 *  @overview       : printing table of power of 2. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 17-02-2020
+	 ******************************************************************************/
 const readline = require('readline-sync');
 const callfunction = require('./power2BL');
 try {
