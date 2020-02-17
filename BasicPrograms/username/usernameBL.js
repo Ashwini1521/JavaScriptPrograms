0185bc7c99b50d1967d1082c50ee853c360c0015
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 15:03:52 +0530

    [Ashwini]refactor: indented business logic of username program

--- BasicPrograms/username/usernameBL.js
+++ BasicPrograms/username/usernameBL.js
@@ -1,16 +1,14 @@
 const readline = require('readline-sync');
 module.exports = {
-    checkusername:(username) => {
+    checkusername: (username) => {
         var len = username.length;
-        if (len <3)
-        {
+        if (len < 3) {
             console.log("Please enter minimum 3 character!");
             let username = readline.question("Enter your name : ");
             let result = module.exports.checkusername(username);
         }
-        // else
-        else{
-            console.log("Hello " +username+  ", How are you?");
+        else {
+            console.log("Hello " + username + ", How are you?");
         }
     }
 }
