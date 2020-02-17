3473cb00079a2ec082c0b6c53bd91bbfe6b1f13c
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:08:00 +0530

    [Ashwini] refactor: indented the main method of binary search program

--- algorithmprogram/BinarySearch/binaryMain.js
+++ algorithmprogram/BinarySearch/binaryMain.js
@@ -15,22 +15,22 @@
 	 *  @version        : 1.0
 	 *  @since          : 13-02-2020
 	 ******************************************************************************/
-    const readline = require('readline-sync');
-    const callfunction = require('./binaryBL');
-    
-    
-    const fs=require('fs');
-    async function test() {
+const readline = require('readline-sync');
+const callfunction = require('./binaryBL');
+
+
+const fs = require('fs');
+async function test() {
     try {
-    let file = await callfunction.filereading();
-    console.log("in main",file);
-    let string1=readline.question("Enter the string to search : ");
-    let ans = callfunction.binarysearch(string1,file);
-    console.log(ans);
+        let file = await callfunction.filereading();
+        console.log("in main", file);
+        let string1 = readline.question("Enter the string to search : ");
+        let ans = callfunction.binarysearch(string1, file);
+        console.log(ans);
     }
     catch (error) {
-    console.log(error);
+        console.log(error);
     }
-    }
-    
-    test();
+}
+
+test();
