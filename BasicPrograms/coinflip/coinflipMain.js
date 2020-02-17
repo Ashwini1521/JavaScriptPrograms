e71e808470368a04c28e331a6ffd7cfdf85f94ac
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:13:07 +0530

    [Ashwini] refactor:indented main method of coinflip program

--- BasicPrograms/coinflip/coinflipMain.js
+++ BasicPrograms/coinflip/coinflipMain.js
@@ -17,11 +17,11 @@ const readline = require('readline-sync');
 const callfunction = require('./coinflipBL');
 try {
     let number = readline.questionInt("enter how many times you want to flip the coin : ");
-    let result=callfunction.flip(number);
-   
-   console.log(result.headpercent);
-   console.log(result.tailpercent);
-   console.log(Math.random()*2+0);
+    let result = callfunction.flip(number);
+
+    console.log(result.headpercent);
+    console.log(result.tailpercent);
+    console.log(Math.random() * 2 + 0);
 } catch (error) {
-    
+
 }
