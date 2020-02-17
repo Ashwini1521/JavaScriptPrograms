692e440d233d7cda614bb76200dca11fce34972b
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:15:56 +0530

    [Ashwini] refactor: indented main method of primefactor program

--- BasicPrograms/factor/factorMain.js
+++ BasicPrograms/factor/factorMain.js
@@ -17,8 +17,8 @@ const readline = require('readline-sync');
 const callfunction = require('./factorBL');
 try {
     let number = readline.questionInt("Enter number :");
-   let result= callfunction.factor(number);
-   console.log(result);
+    let result = callfunction.factor(number);
+    console.log(result);
 
 } catch (error) {
     console.log(error);
