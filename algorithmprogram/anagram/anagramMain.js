ca362e000df39d69968831fb3d64892681b96fd6
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:05:31 +0530

    [Ashwini] refactor:indented the Main method of anagram program

--- algorithmprogram/anagram/anagramMain.js
+++ algorithmprogram/anagram/anagramMain.js
@@ -3,15 +3,14 @@ let callFunction = require('./anagramBL');
 try {
     let String1 = readline.question('enter first string: ');
     let String2 = readline.question('enter second string: ');
-    var res=callFunction.checkanagram(String1, String2);
-    if (res)
-    {
+    var res = callFunction.checkanagram(String1, String2);
+    if (res) {
         console.log("The above Strings are anagram");
-        }
-        else {
-            console.log("The above Strings are not anagram");
-        }
+    }
+    else {
+        console.log("The above Strings are not anagram");
+    }
 } catch (error) {
- console.log(error);
- 
+    console.log(error);
+
 }
