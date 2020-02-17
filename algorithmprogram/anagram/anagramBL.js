6f69a4b4b40d40d4c5772d7734423e96de279227
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:06:13 +0530

    [Ashwini] refactor:indented the business logic of anagram program

--- algorithmprogram/anagram/anagramBL.js
+++ algorithmprogram/anagram/anagramBL.js
@@ -2,8 +2,8 @@ module.exports = {
     checkanagram: (String1, String2) => {
         console.log("String1:" + String1);
         console.log("String2:" + String2);
-        return String1.split("").sort().join("") === String2.split("").sort().join("");   
-    
+        return String1.split("").sort().join("") === String2.split("").sort().join("");
+
     }
 
 
