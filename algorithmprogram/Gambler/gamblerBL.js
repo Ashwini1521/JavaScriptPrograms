9c35083f221102409785578120d7c8a5b8ff2b4c
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:04:05 +0530

    [Ashwini] refactor:indented the business logic of gambler program

--- algorithmprogram/Gambler/gamblerBL.js
+++ algorithmprogram/Gambler/gamblerBL.js
@@ -1,22 +1,22 @@
 module.exports = {
     gambling: (stake, goal, Nooftrails) => {
-    var playCount = 0;
-    var win = 0;
-    while ((stake > 0 || stake < goal) && Nooftrails > 0) {
-    var rand = Math.random();
-    console.log(rand);
-    if (rand > 0.5) {
-    win++;
-    stake++;
+        var playCount = 0;
+        var win = 0;
+        while ((stake > 0 || stake < goal) && Nooftrails > 0) {
+            var rand = Math.random();
+            console.log(rand);
+            if (rand > 0.5) {
+                win++;
+                stake++;
+            }
+            else {
+                stake--;
+            }
+            console.log("current amount is: " + stake);
+            Nooftrails--;
+            playCount++;
+        }
+        console.log("no. of time played: " + playCount);
+        console.log("percentage of win: " + (win / playCount * 100) + "%");
     }
-    else {
-    stake--;
-    }
-    console.log("current amount is: " + stake);
-    Nooftrails--;
-    playCount++;
-    }
-    console.log("no. of time played: " + playCount);
-    console.log("percentage of win: " + (win / playCount * 100) + "%");
-    }
-    }
+}
