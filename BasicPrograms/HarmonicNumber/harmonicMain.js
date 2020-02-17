a7fc4ec3c32c7448546d14271319e861351f2fd0
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 09:52:54 +0530

    [Ashwini] refactor:commented on main method of harmonic number

--- BasicPrograms/HarmonicNumber/harmonicMain.js
+++ BasicPrograms/HarmonicNumber/harmonicMain.js
@@ -1,3 +1,19 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node harmonicMain.js
+	 *                      2. if nodemon installed cmd> nodemon harmonicMain.js
+	 *
+	 *  Purpose         : to find harmonic number.
+	 *
+	 *  @description    : taking input from user and applying it into the given formula to find the harmonic number...
+	 *
+	 *  @file           : harmonicMain.js
+	 *  @overview       : find the harmonic number from  given input. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 17-02-2020
+	 ******************************************************************************/
+	
 const readline = require('readline-sync');
 const callFunction = require('./harmonicBL');
 try {
