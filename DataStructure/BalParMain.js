a5251060c5fb4a635e9ff844f0ae325e5d47f698
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 18:34:06 +0530

    [Ashwini] refactor:commented Main method of balance paranthesis program

--- DataStructure/BalParMain.js
+++ DataStructure/BalParMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node BalParMain.js
+	 *                      2. if nodemon installed cmd> nodemon BalParMain.js
+	 *
+	 *  Purpose         : balance paranthesis.
+	 *
+	 *  @description    : to check if the paranthesis are balanced or not...
+	 *
+	 *  @file           : BalParMain.js
+	 *  @overview       : checking balance paranthesis. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 17-02-2020
+	 ******************************************************************************/
 const readline = require('readline-sync');
 const callfunction = require('./BalParBL');
 try {
