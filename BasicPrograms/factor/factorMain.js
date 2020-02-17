792e3a98dd67563bc374b97202ed4f61526e4711
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 10:06:09 +0530

    [Ashwini] refactor:commented on Main method of leap year program

--- BasicPrograms/factor/factorMain.js
+++ BasicPrograms/factor/factorMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node factorMain.js
+	 *                      2. if nodemon installed cmd> nodemon factorMain.js
+	 *
+	 *  Purpose         : finding primefactorisation. 
+	 *
+	 *  @description    : Computes the prime factorization of N using brute force...
+	 *
+	 *  @file           : factorMain.js
+	 *  @overview       : primefactorisation. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 17-02-2020
+	 ******************************************************************************/
 const readline = require('readline-sync');
 const callfunction = require('./factorBL');
 try {
