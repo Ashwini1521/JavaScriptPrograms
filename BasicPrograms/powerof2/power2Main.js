8bd3b8bd86bf28f51a56bf0ebf10ca45ad6a2c7c
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 15:01:08 +0530

    [Ashwini] refactor: indented main method of power of 2 program

--- BasicPrograms/powerof2/power2Main.js
+++ BasicPrograms/powerof2/power2Main.js
@@ -16,10 +16,10 @@
 const readline = require('readline-sync');
 const callfunction = require('./power2BL');
 try {
-    let number = readline.questionInt("Enter number :");
-   let result= callfunction.power2(number);
-   console.log(result);
+	let number = readline.questionInt("Enter number :");
+	let result = callfunction.power2(number);
+	console.log(result);
 
 } catch (error) {
-    console.log(error);
+	console.log(error);
 }
