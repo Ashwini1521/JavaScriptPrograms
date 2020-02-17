0bed99a8f09e7cd01bab1e699ed49e03f8dd4e5d
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:11:32 +0530

    [Ashwini] refactor:indented business logic of coupon number program

--- algorithmprogram/couponNumber/couponBL.js
+++ algorithmprogram/couponNumber/couponBL.js
@@ -1,19 +1,19 @@
 module.exports = {
     CouponNumber: (coupon) => {
-       
-        let NoOfRandomCall =0
-        let length=coupon.length;
-        while(coupon.length > 0){
 
-            let index=-1;
-            let num = Math.floor(Math.random()*100)+1;
+        let NoOfRandomCall = 0
+        let length = coupon.length;
+        while (coupon.length > 0) {
+
+            let index = -1;
+            let num = Math.floor(Math.random() * 100) + 1;
             NoOfRandomCall++;
-            index = coupon.indexOf(num,0);
+            index = coupon.indexOf(num, 0);
 
-            if(index>-1){
-                coupon.splice(index,1);
+            if (index > -1) {
+                coupon.splice(index, 1);
             }
-            
+
         }
         return NoOfRandomCall;
     }
