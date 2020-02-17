0bffda1f159a336d982b4f42398237a3ae0b60eb
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 15:03:10 +0530

    [Ashwini]refactor: indented main method of username program

--- BasicPrograms/username/usernameMain.js
+++ BasicPrograms/username/usernameMain.js
@@ -16,8 +16,8 @@
 const readline = require('readline-sync');
 const callfunction = require('./usernameBL');
 try {
-    let username = readline.question("Enter your name : ");
-    let result = callfunction.checkusername(username);
+	let username = readline.question("Enter your name : ");
+	let result = callfunction.checkusername(username);
 } catch (error) {
-    console.log(error);
+	console.log(error);
 }
