15a7b3120815106f5a65dfef7f810d67814f2902
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 13:16:44 +0530

    [Ashwini] refactor: indented business logic of primefactor program

--- BasicPrograms/factor/factorBL.js
+++ BasicPrograms/factor/factorBL.js
@@ -1,20 +1,20 @@
 module.exports = {
-    factor:(number) => {
-         var n= number;
-         var a=[];
-         var count=0;
-         
-         for (i=2;i<=n;i++){
-            while (n%i == 0){
-                n=n/i;              
-                    a[count++]=i;
-                }
+    factor: (number) => {
+        var n = number;
+        var a = [];
+        var count = 0;
+
+        for (i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                n = n / i;
+                a[count++] = i;
             }
-            if (n==1){
-                return a;
-            }
-            
-         }
-        
+        }
+        if (n == 1) {
+            return a;
+        }
+
     }
+
+}
 
