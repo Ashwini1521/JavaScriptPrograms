9b9bec2a69223524f0f50e2b6250885142950770
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 18:27:32 +0530

    [Ashwini] Added Main method of balance paranthesis program

---
+++ DataStructure/BalParMain.js
@@ -0,0 +1,9 @@
+const readline = require('readline-sync');
+const callfunction = require('./BalParBL');
+try {
+   let expression=readline.question("enter the Arthemetic expression")
+ let result = callfunction.isBalanced(expression);
+ console.log(result);
+} catch (err) {
+   throw err;
+}
