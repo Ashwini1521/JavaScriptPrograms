d8516578fb02dc5e84f9c93b7f262004136928a5
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 14:56:12 +0530

    [Ashwini] refactor: indented main method of harmonic number program

--- BasicPrograms/HarmonicNumber/harmonicMain.js
+++ BasicPrograms/HarmonicNumber/harmonicMain.js
@@ -13,14 +13,14 @@
 	 *  @version        : 1.0
 	 *  @since          : 17-02-2020
 	 ******************************************************************************/
-	
+
 const readline = require('readline-sync');
 const callFunction = require('./harmonicBL');
 try {
-let harmonicnumber = readline.questionInt("enter number: ");
-let result = callFunction.checkharmonic(harmonicnumber);
-console.log(result);
+	let harmonicnumber = readline.questionInt("enter number: ");
+	let result = callFunction.checkharmonic(harmonicnumber);
+	console.log(result);
 }
 catch (error) {
-console.log(error);
+	console.log(error);
 }
