f93313b7a06e957c4f4cd67285f5e5f80b92aced
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 15:01:52 +0530

    [Ashwini] refactor: indented business logic of power of 2 program

--- BasicPrograms/powerof2/power2BL.js
+++ BasicPrograms/powerof2/power2BL.js
@@ -1,14 +1,14 @@
 module.exports = {
-    power2:(number) => {
-        let power=1;
-        if ( 0 <= number && number < 31 ) {
-            for ( i=1; i<=number; i++ ) {
-                power=power*2;
-                console.log("2^" +i+" "+power);
+    power2: (number) => {
+        let power = 1;
+        if (0 <= number && number < 31) {
+            for (i = 1; i <= number; i++) {
+                power = power * 2;
+                console.log("2^" + i + " " + power);
 
             }
         }
-        else{
+        else {
             console.log("Inavalid input");
         }
 
