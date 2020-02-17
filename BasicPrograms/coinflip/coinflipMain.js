a2020c4e70535b6a7ea978164b53110a775aad9c
Dilip138 <dilipsoni199566@gmail.com>
2020-02-17 10:09:20 +0530

    [Ashwini] refactor:commented on Main method of leap year program

--- BasicPrograms/coinflip/coinflipMain.js
+++ BasicPrograms/coinflip/coinflipMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node coinflipMain.js
+	 *                      2. if nodemon installed cmd> nodemon coinflipMain.js
+	 *
+	 *  Purpose         : to find the percentage of heads and tails. 
+	 *
+	 *  @description    :  fliping the coin and printing the percentage of Heads and Tails...
+	 *
+	 *  @file           : coinflipMain.js
+	 *  @overview       : percentage of heads and tails. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 17-02-2020
+	 ******************************************************************************/
 const readline = require('readline-sync');
 const callfunction = require('./coinflipBL');
 try {
@@ -6,6 +21,7 @@ try {
    
    console.log(result.headpercent);
    console.log(result.tailpercent);
+   console.log(Math.random()*2+0);
 } catch (error) {
     
 }
