2d3ca8bee84fd1a14a7fa901b10538446f1ff4e6
Ashwini1521 <ashwiniswamy1521@gamil.com>
2020-02-18 12:56:15 +0530

    [Ashwini] refactor:commented Main method of bubblesort

--- algorithmprogram/bubblesort/bubblesortMain.js
+++ algorithmprogram/bubblesort/bubblesortMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node bubblesortMain.js
+	 *                      2. if nodemon installed cmd> nodemon bubblesortMain.js
+	 *
+	 *  Purpose         : usage of bubblesort .
+	 *
+	 *  @description    : taking the array elements from user and sorting those array elements using bubblesort...
+	 *
+	 *  @file           : bubblesortMain.js
+	 *  @overview       : find the harmonic number from  given input. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 18-02-2020
+	 ******************************************************************************/
 let readline = require('readline-sync');
 let callFunction = require('./bubblesortBL');
 try {
