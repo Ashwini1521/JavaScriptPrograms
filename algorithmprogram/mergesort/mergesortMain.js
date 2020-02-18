71b1f7dec8fc778be58208bd2eaa37844f310d39
Ashwini1521 <ashwiniswamy1521@gamil.com>
2020-02-18 13:13:05 +0530

    [Ashwini] refactor: commented Main method of mergesort

--- algorithmprogram/mergesort/mergesortMain.js
+++ algorithmprogram/mergesort/mergesortMain.js
@@ -1,9 +1,24 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node mergesortMain.js
+	 *                      2. if nodemon installed cmd> nodemon mergesortMain.js
+	 *
+	 *  Purpose         : usage of mergesort .
+	 *
+	 *  @description    : taking the array elements from user and sorting those array elements using mergesort...
+	 *
+	 *  @file           : mergesortMain.js
+	 *  @overview       : find the harmonic number from  given input. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 1.0
+	 *  @since          : 18-02-2020
+	 ******************************************************************************/
 let readline = require('readline-sync');
 let callFunction = require('./mergesortBL');
 try {
-    var arr = [12,4,3,15,10,9];
+    var arr = [12, 4, 3, 15, 10, 9];
     console.log("arr");
-    let result = callFunction.mergesort(arr, 0, arr.length-1);
+    let result = callFunction.mergesort(arr, 0, arr.length - 1);
     return result;
 } catch (error) {
     console.log(error);
