b910a7292f3518367b07f524293c014b74376e01
AkulaSravanthi <sravanthiakula15@gmail.com>
2020-02-20 10:57:31 +0530

    [Ashwini] Added. business logic  of distance

---
+++ functional/Distance/distanceBL.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //Find And Print distance between point ti origin
+
+    distance: (x2, y2) => {
+        return Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
+        //return distance.
+    }
+}
