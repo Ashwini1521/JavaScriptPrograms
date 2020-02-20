18d3866bf64752cb1e69ff99a2633fc7a19a6335
AkulaSravanthi <sravanthiakula15@gmail.com>
2020-02-20 10:11:32 +0530

    [Ashwini] refactor. commented on  Main method

--- functional/Quadratic/quadraticMain.js
+++ functional/Quadratic/quadraticMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node quadraticMain.js
+	 *                      2. if nodemon installed cmd> nodemon quadraticMain.js
+	 *
+	 *  Purpose         : to find the roots. 
+	 *
+	 *  @description    :  to find the roots using quadratic equation...
+	 *
+	 *  @file           : quadraticMain.js
+	 *  @overview       : positive and negative roots using quadratic. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 20-02-2020
+	 ******************************************************************************/
 const readLine = require('readline-sync');
 const quadraticBL = require('./quadraticBL');
 try {
