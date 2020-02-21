29c8d4c43a5d5cae1dbab0e88c18039fd4d359ec
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-21 18:17:53 +0530

    [Ashwini] refactor: commented main method

--- DataStructure/2Dprimenumbers/2DprimeMain.js
+++ DataStructure/2Dprimenumbers/2DprimeMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node 2DprimeMain.js
+	 *                      2. if nodemon installed cmd> nodemon 2DprimeMain.js
+	 *
+	 *  Purpose         : prime number in 2DArray.. 
+	 *
+	 *  @description    :to print prime numbers in 2Darray...
+	 *
+	 *  @file           : 2DprimeMain.js
+	 *  @overview       : prime number in 2DArray. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 21-02-2020
+	 ******************************************************************************/
 let read = require('readline-sync');
 const callFunction = require('./2DprimeBL');
 try{
