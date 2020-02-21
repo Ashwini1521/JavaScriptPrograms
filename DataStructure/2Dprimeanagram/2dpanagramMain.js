9044f70cbd9b5f122532e994ceeab2f64983b3c8
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-21 18:14:09 +0530

    [Ashwini] refactor: commented main method

--- DataStructure/2Dprimeanagram/2dpanagramMain.js
+++ DataStructure/2Dprimeanagram/2dpanagramMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node 2dpanagramMain.js
+	 *                      2. if nodemon installed cmd> nodemon 2dpanagramMain.js
+	 *
+	 *  Purpose         : to print prime anagram numbers. 
+	 *
+	 *  @description    :to print prime numbers as well as anagram of that array...
+	 *
+	 *  @file           : 2dpanagramMain.js
+	 *  @overview       : prime anagram. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 21-02-2020
+	 ******************************************************************************/
 const readline = require('readline-sync');
 const callFunction = require('./2dpanagramBL');
 
