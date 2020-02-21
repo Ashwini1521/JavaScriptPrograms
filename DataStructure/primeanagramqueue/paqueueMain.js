20968a3e8a17f4e08f2d8c22b92cce992621310d
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-21 18:07:21 +0530

    [Ashwini] refactor: commented main method

--- DataStructure/primeanagramqueue/paqueueMain.js
+++ DataStructure/primeanagramqueue/paqueueMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node paqueueMain.js
+	 *                      2. if nodemon installed cmd> nodemon paqueueMain.js
+	 *
+	 *  Purpose         : to print prime anagram numbers in queue. 
+	 *
+	 *  @description    :to print prime numbers as well as anagram in given range and then  it has to store in queue ...
+	 *
+	 *  @file           : paqueueMain.js
+	 *  @overview       : queue . 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 21-02-2020
+	 ******************************************************************************/
 const readline = require('readline-sync');
 const callFunction = require('./paqueueBL');
 
