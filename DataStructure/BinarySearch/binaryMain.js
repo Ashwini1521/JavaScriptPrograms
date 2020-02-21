10f0a15729890b1914c3529e3fdc770c46ee519c
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-21 09:43:46 +0530

    [Ashwini] refactor: commented methods of binary search

--- DataStructure/BinarySearch/binaryMain.js
+++ DataStructure/BinarySearch/binaryMain.js
@@ -1,12 +1,27 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node binaryMain.js
+	 *                      2. if nodemon installed cmd> nodemon binaryMain.js
+	 *
+	 *  Purpose         : to print binarysearchtree. 
+	 *
+	 *  @description    :to print inorder and preorder of userinput using binarysearchtree ...
+	 *
+	 *  @file           : binaryMain.js
+	 *  @overview       : binarysearchtree. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 21-02-2020
+	 ******************************************************************************/
 const BstLib = require('./binaryBL');
 try {
     let main = () => {
         let BST = new BstLib.binarySearchTree();
+        BST.insert(7);
         BST.insert(2);
-        BST.insert(5);
-        BST.insert(3);
-        BST.insert(1);
         BST.insert(9);
+        BST.insert(4);
+        BST.insert(5);
         let rootNode = BST.getRootNode();
         // console.log(root);
         console.log('inorder');
