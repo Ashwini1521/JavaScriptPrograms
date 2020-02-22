855f3697a5e733196c5644bbdec9b5fbc7e2f17b
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-22 16:39:50 +0530

    [Ashwini] refactor:commented main method of calendar

--- DataStructure/Calender/calenderMain.js
+++ DataStructure/Calender/calenderMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node calenderMain.js
+	 *                      2. if nodemon installed cmd> nodemon calenderMain.js
+	 *
+	 *  Purpose         : to print calender. 
+	 *
+	 *  @description    :to print calender with day of week and date , year using 2Darray...
+	 *
+	 *  @file           : calenderMain.js
+	 *  @overview       : calender  . 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 22-02-2020
+	 ******************************************************************************/
 const readline = require('readline-sync');
 const callfunction = require('./calenderBL');
 try {
