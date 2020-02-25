4a9e0fa23b4ecb48fefdef667e45071c2158cfe9
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-25 19:25:33 +0530

    [Ashwini] Added test

---
+++ Test/usernametest.js
@@ -0,0 +1,8 @@
+console.log("getting into the test file");
+const assert=require('chai').assert;
+const UN=require('../BasicPrograms/username');
+describe('Check username from UN', () => {
+    it('should be greaterthan 3',() => {
+        assert.equal(UN.username(),'name');
+    })
+})
