bfe96d63bc84a276aa57dbf1cc4db08cac746524
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-26 11:17:17 +0530

    [Ashwini] Added test case of leapyear

---
+++ Test/leaptest.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert; //assert to determine the status of the failure
+const leap = require("../BasicPrograms/leap");
+describe('get if leap year or not from leap.js', () => { //holds collection of test
+    it('should be a 4 digit number', () => { //it() is the test itself
+
+        assert.equal(leap.isLeap(2019), false);
+    });
+    it("should not be negative", () => {
+        assert.equal(leap.isLeap(-1929), false);
+    });
+    it("should not be decimal number", () => {
+        assert.equal(leap.isLeap(1.2), false);
+    });
+    it("should not be a string or a character", () => {
+        assert.equal(leap.isLeap("abdc"), false);
+    });
+    it("should not be less than 4 digits", () => {
+        assert.equal(leap.isLeap(193), false);
+    });
+    it("for leap year should return true", () => {
+        assert.equal(leap.isLeap(1600), true);
+    });
+});
