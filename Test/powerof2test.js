cc3175a90ee3e5fc4cde071437ede72137bf1c82
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-26 12:17:58 +0530

    [Ashwini] refactor commented on test case

--- Test/powerof2test.js
+++ Test/powerof2test.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node powerof2test.js
+	 *                      2. if nodemon installed cmd> nodemon powerof2test.js
+	 *
+	 *  Purpose         : testing.. 
+	 *
+	 *  @description    : taking input and returns power...
+	 *
+	 *  @file           : powerof2test.js
+	 *  @overview       : unittest using mocha chai. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 26-02-2020
+	 ******************************************************************************/
 const assert = require('chai').assert; //assert to determine the status of the failure
 const power = require("../BasicPrograms/powerof2");
 describe('get powerof2 powerof2.js', () => { //holds collection of test
