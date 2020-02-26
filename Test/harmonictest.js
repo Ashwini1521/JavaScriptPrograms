e9a4241a5ef8f5823125c4098366d0064fd4755d
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-26 12:14:37 +0530

    [Ashwini] refactor commented on test case

--- Test/harmonictest.js
+++ Test/harmonictest.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node harmonictest.js
+	 *                      2. if nodemon installed cmd> nodemon harmonictest.js
+	 *
+	 *  Purpose         : testing.. 
+	 *
+	 *  @description    : taking input and returns harmonic sum...
+	 *
+	 *  @file           : harmonictest.js
+	 *  @overview       : unittest using mocha chai. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 26-02-2020
+	 ******************************************************************************/
 const assert = require('chai').assert;
 const HN = require('../BasicPrograms/harmonic');
 describe("Harmonic", () => {
