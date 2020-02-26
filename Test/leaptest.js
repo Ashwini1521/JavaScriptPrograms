79e68ce6a8db0a327ea4c2aa0834f52f28c07b75
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-26 12:16:38 +0530

    [Ashwini] refactor commented on test case

--- Test/leaptest.js
+++ Test/leaptest.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node leaptest.js
+	 *                      2. if nodemon installed cmd> nodemon leaptest.js
+	 *
+	 *  Purpose         : testing.. 
+	 *
+	 *  @description    : taking input and returns leapyear...
+	 *
+	 *  @file           : leaptest.js
+	 *  @overview       : unittest using mocha chai. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 26-02-2020
+	 ******************************************************************************/
 const assert = require('chai').assert; //assert to determine the status of the failure
 const leap = require("../BasicPrograms/leap");
 describe('get if leap year or not from leap.js', () => { //holds collection of test
