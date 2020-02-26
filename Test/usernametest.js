53207e644f50a9ff6b6dacada4a8c6dfc0ad419a
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-26 12:20:07 +0530

    [Ashwini] refactor commented on test case

--- Test/usernametest.js
+++ Test/usernametest.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node usernametest.js
+	 *                      2. if nodemon installed cmd> nodemon usernametest.js
+	 *
+	 *  Purpose         : testing.. 
+	 *
+	 *  @description    : taking input and checks the length of name and returns hello...
+	 *
+	 *  @file           : usernametest.js
+	 *  @overview       : unittest using mocha chai. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 26-02-2020
+	 ******************************************************************************/
 console.log("getting into the test file");
 const assert = require('chai').assert;
 const UN = require('../BasicPrograms/username');
