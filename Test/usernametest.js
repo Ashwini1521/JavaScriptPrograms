62d8b7390882c26c40542c266709e71830164f46
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-26 11:02:16 +0530

    [Ashwini] Refactor:indented test case of username

--- Test/usernametest.js
+++ Test/usernametest.js
@@ -1,8 +1,8 @@
 console.log("getting into the test file");
-const assert=require('chai').assert;
-const UN=require('../BasicPrograms/username');
+const assert = require('chai').assert;
+const UN = require('../BasicPrograms/username');
 describe('Check username from UN', () => {
-    it('should be greaterthan 3',() => {
-        assert.equal(UN.username(),'name');
+    it('should be greaterthan 3', () => {
+        assert.equal(UN.username(), 'name');
     })
 })
