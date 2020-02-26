22a8560e11c805f316a4d6b3f461c834f823ef90
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-26 12:05:49 +0530

    [Ashwini] Refactor business logic of harmonic

--- BasicPrograms/harmonic.js
+++ BasicPrograms/harmonic.js
@@ -1,7 +1,8 @@
 module.exports = {
     getHarmonicNum: (num) => {
+        var undefined = null;
         if (Math.floor(num) != num || num <= 0)
-            return "undefiend";
+            return "undefined";
         let sum = 0.0;
         while (num > 0) {
             sum += 1 / num;
