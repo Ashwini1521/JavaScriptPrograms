6af1dd78b0d6e12571b9b475a9f6fc1e70381f5c
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-02-27 16:51:56 +0530

    [Ashwini] Added business logic

--- InventriesManagement/InvManBL.js
+++ InventriesManagement/InvManBL.js
@@ -1,50 +1,49 @@
-
 const readLine = require('readline-sync');
 const fs = require('fs');
 /**
  * @class Rice
 */
-class Rice{
-    constructor(name,weight,price){
+class Rice {
+    constructor(name, weight, price) {
         this.name = name;
         this.weight = weight;
         this.price = price;
     }
-    toString(){
+    toString() {
         console.log(`${this.name} - ${this.weight} - ${this.price}`);
     }
 }
 /**
  * @class Pulses
 */
-class Pulses{
-    constructor(name,weight,price){
+class Pulses {
+    constructor(name, weight, price) {
         this.name = name;
         this.weight = weight;
         this.price = price;
     }
-    toString(){
+    toString() {
         console.log(`${this.name} - ${this.weight} - ${this.price}`);
     }
 }
 /**
  * @class Wheats
 */
-class Wheats{
-    constructor(name,weight,price){
+class Wheats {
+    constructor(name, weight, price) {
         this.name = name;
         this.weight = weight;
         this.price = price;
     }
-    toString(){
+    toString() {
         console.log(`${this.name} - ${this.weight} - ${this.price}`);
     }
 }
 /**
  * @class ManageInventory
 */
-class ManageInventory{
-    constructor(){
+class ManageInventory {
+    constructor() {
         this.rice = [];
         this.pulses = [];
         this.wheats = [];
@@ -52,77 +51,77 @@ class ManageInventory{
         let inventoryData = JSON.parse(jsonData);
         this.inventories = {};
         //it check array is there or not in object.
-        if(Array.isArray(inventoryData.rice)){
+        if (Array.isArray(inventoryData.rice)) {
             this.inventories.rice = inventoryData.rice;
         } else {
             // Object.assign(newObject,oldObject) it append new-Object to old-object. 
-         this.inventories = Object.assign({rice:[]},this.inventories)
-         }
-        if(Array.isArray(inventoryData.pulses)){
+            this.inventories = Object.assign({ rice: [] }, this.inventories)
+        }
+        if (Array.isArray(inventoryData.pulses)) {
             this.inventories.pulses = inventoryData.pulses;
         } else {
-            this.inventories = Object.assign({pulses:[]},this.inventories) 
+            this.inventories = Object.assign({ pulses: [] }, this.inventories)
         }
-        if(Array.isArray(inventoryData.wheats)){
+        if (Array.isArray(inventoryData.wheats)) {
             this.inventories.wheats = inventoryData.wheats;
         } else {
-            this.inventories = Object.assign({wheats:[]},this.inventories)
+            this.inventories = Object.assign({ wheats: [] }, this.inventories)
         }
     }
     /**
      * @module-it will insert the RiceData in the Inventory.json file. 
     */
-    fillRice(){
+    fillRice() {
         const name = readLine.question('Enter the Rice Name : ');
         const weight = readLine.questionFloat('Enter the weight of Rice : ');
         const price = readLine.questionFloat('Enter the price of Rice : ');
-        const rice = new Rice(name,weight,price);
+        const rice = new Rice(name, weight, price);
         this.inventories.rice.push(JSON.parse(JSON.stringify(rice)));
-        fs.writeFileSync('InvMan.json',JSON.stringify(this.inventories));
+        fs.writeFileSync('InvMan.json', JSON.stringify(this.inventories));
     }
     /**
      * @module-it will insert the PulsesData in the Inventory.json file. 
     */
-    fillPulses(){
+    fillPulses() {
         const name = readLine.question('Enter the Name of pulses : ');
         const weight = readLine.questionFloat('Enter the weight of pulses : ');
         const price = readLine.questionFloat('Enter the price of pulses : ');
-        const pulses = new Pulses(name,weight,price);
+        const pulses = new Pulses(name, weight, price);
         this.inventories.pulses.push(JSON.parse(JSON.stringify(pulses)));
-        fs.writeFileSync('InvMan.json',JSON.stringify(this.inventories));
+        fs.writeFileSync('InvMan.json', JSON.stringify(this.inventories));
     }
     /**
      * @module-it will insert the WheateData in the Inventory.json file. 
     */
-    fillWheats(){
+    fillWheats() {
         const name = readLine.question('Enter the name of wheat : ');
         const weight = readLine.questionFloat('Enter the weight of wheat :');
         const price = readLine.questionFloat('Enter the price of wheat : ');
-        const wheat = new Wheats(name,weight,price);
+        const wheat = new Wheats(name, weight, price);
         this.inventories.wheats.push(JSON.parse(JSON.stringify(wheat)));
-        fs.writeFileSync('InvMan.json',JSON.stringify(this.inventories));
+        fs.writeFileSync('InvMan.json', JSON.stringify(this.inventories));
     }
     /**
      * @module-it will calculate and print each inventories price. 
     */
-    calculateInventoryTotal(){
+    calculateInventoryTotal() {
         let riceTotal = 0;
         let plusesTotal = 0;
         let wheatsTotal = 0;
-        for(let i=0;i<this.inventories.rice.length;i++){
+        for (let i = 0; i < this.inventories.rice.length; i++) {
             riceTotal += this.inventories.rice[i].price;
         }
-        for(let i=0;i<this.inventories.pulses.length;i++){
+        for (let i = 0; i < this.inventories.pulses.length; i++) {
             plusesTotal += this.inventories.pulses[i].price;
         }
-        for(let i=0;i<this.inventories.wheats.length;i++){
+        for (let i = 0; i < this.inventories.wheats.length; i++) {
             wheatsTotal += this.inventories.wheats[i].price;
         }
         console.log(`Total price of Rice = ${riceTotal}`);
         console.log(`Total price of Pluses = ${plusesTotal}`);
         console.log(`Total price of Wheats = ${wheatsTotal}`);
     }
-    inventoryObject(){
+    inventoryObject() {
         console.log('-------RICE OBJECT---------');
         console.log(this.inventories.rice);
         console.log('-------PLUSES OBJECT-------');
