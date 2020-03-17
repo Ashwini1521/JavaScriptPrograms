264767a47ad005760b21e2f1e092652abb789021
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-03-17 19:08:29 +0530

    [Ashwini] refactor:commented on Main method of factory design pattern

--- JSP/DesignPatterns/FactoryPattern/factoryMain.js
+++ JSP/DesignPatterns/FactoryPattern/factoryMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node factoryMain.js
+	 *                      2. if nodemon installed cmd> nodemon factoryMain.js
+	 *
+	 *  Purpose         : to design factory. 
+	 *
+	 *  @description    :designed  college factory with name and type ...
+	 *
+	 *  @file           : factoryMain.js
+	 *  @overview       : factory design. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 17-03-2020
+	 ******************************************************************************/
 const callfunction = require('./factoryBl');
 try {
     let clgfactory = new callfunction.collegefactory();
@@ -6,11 +21,11 @@ try {
     college.push(clgfactory.create("Navya", "Professor"));
     college.push(clgfactory.create("Ashwini", "Student"));
     college.push(clgfactory.create("Mamta", "Student"));
-   
+
     for (let clg in college) {
         console.log(`Hi I am ${college[clg].name} And I am a ${college[clg].type}`);
     }
-    
+
 } catch (error) {
     throw error;
 }
