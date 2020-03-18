98f3b2743940d247295460410a0e803af0619959
unknown <ashwiniswamy1521@gmail.com>
2020-03-18 19:39:29 +0530

    [Ashwini] refactor:commented on main method of prototype design pattern

--- JSP/DesignPatterns/PrototypePattern/prototypeMain.js
+++ JSP/DesignPatterns/PrototypePattern/prototypeMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node prototypeMain.js
+	 *                      2. if nodemon installed cmd> nodemon prototypeMain.js
+	 *
+	 *  Purpose         : to design Prototype pattern. 
+	 *
+	 *  @description    :designed  prototype pattern ...
+	 *
+	 *  @file           : prototypeMain.js
+	 *  @overview       : prototype pattern. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 18-03-2020
+	 ******************************************************************************/
 const callfunction = require('./prototypeBl')
 try {
 
