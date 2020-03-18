cbe5b0a1c85d50e5d014a9477d5162d5dc3b312e
unknown <ashwiniswamy1521@gmail.com>
2020-03-18 19:38:41 +0530

    [Ashwini] refactor:commented on main method of observer design pattern

--- JSP/DesignPatterns/ObserverPattern/observerMain.js
+++ JSP/DesignPatterns/ObserverPattern/observerMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node observerMain.js
+	 *                      2. if nodemon installed cmd> nodemon observerMain.js
+	 *
+	 *  Purpose         : to design observer pattern. 
+	 *
+	 *  @description    :designed observer pattern ...
+	 *
+	 *  @file           : observerMain.js
+	 *  @overview       : observer design. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 18-03-2020
+	 ******************************************************************************/
 const callfunction = require("./observerBl");
 try {
     let main = () => {
