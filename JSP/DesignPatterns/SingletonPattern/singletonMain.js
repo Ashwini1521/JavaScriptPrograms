99cebb4753d96eb602b5f003d65f42e38254fd3e
unknown <ashwiniswamy1521@gmail.com>
2020-03-18 19:37:46 +0530

    [Ashwini] refactor:commented on main method of singleton design pattern

--- JSP/DesignPatterns/SingletonPattern/singletonMain.js
+++ JSP/DesignPatterns/SingletonPattern/singletonMain.js
@@ -1,3 +1,18 @@
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node singletonMain.js
+	 *                      2. if nodemon installed cmd> nodemon singletonMain.js
+	 *
+	 *  Purpose         : to design singleton pattern. 
+	 *
+	 *  @description    :designed  singleton pattern ...
+	 *
+	 *  @file           : singletonMain.js
+	 *  @overview       : singleton design. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : v12.16.1
+	 *  @since          : 18-03-2020
+	 ******************************************************************************/
 const callfunction = require('./singletonBl');
 try {
 
