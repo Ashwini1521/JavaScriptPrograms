191c4d87225944458ace80dbbe7580cef171bae3
unknown <ashwiniswamy1521@gmail.com>
2020-03-18 19:51:57 +0530

    [Ashwini] refactor:commented on main business logic observer design pattern

--- JSP/DesignPatterns/ObserverPattern/observerBl.js
+++ JSP/DesignPatterns/ObserverPattern/observerBl.js
@@ -1,25 +1,39 @@
+/**
+ * @class Subject
+ */
 class subject {
     constructor() {
         this.observer = [];
     }
+
+    /**
+     * @function subscribe 
+     */
     subscribe(observer) {
         console.log(observer);
         this.observer.push(observer);
     }
-
+    /**
+     * @function unsubscribe 
+     */
     unsubscribe(observer) {
         let index = this.observer.indexOf(observer);
         if (index > -1) {
             this.observer.slice(index, 1);
         }
     }
-
+    /**
+     * @function notifyAll 
+     */
     notifyAll() {
         for (let o of this.observer) {
             console.log(o, "has been notified");
         }
     }
 }
+/**
+ * @module subject 
+ */
 module.exports = {
     subject
 };
