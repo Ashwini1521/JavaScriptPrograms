495c0f8f6c7b65c5751b7ca23ec2106cc37f9026
unknown <ashwiniswamy1521@gmail.com>
2020-03-18 19:48:56 +0530

    [Ashwini] refactor:commented on main business logic prototype design pattern

--- JSP/DesignPatterns/PrototypePattern/prototypeBl.js
+++ JSP/DesignPatterns/PrototypePattern/prototypeBl.js
@@ -2,17 +2,24 @@ const readline = require('readline-sync');
 var Students = function (size) {
     this.array = [];
     this.size = size;
-
 }
+/**
+ * @function Students
+ */
 Students.prototype.getName = function () {
     return this.array;
 }
-
+/**
+ * @function reqarray
+ */
 var reqarray = function () {
     for (let i = 0; i < this.size; i++) {
         this.array[i] = readline.question("enter Students names : ");
     }
 }
+/**
+ * @function checkifprototype
+ */
 var checkifprototype = function () {
     var array = [];
     var Student = new Students(4);
@@ -24,6 +31,9 @@ var checkifprototype = function () {
     console.log(array);
 
 }
+/**
+ * @module checkifprototype 
+ */
 module.exports = {
     checkifprototype
 }
