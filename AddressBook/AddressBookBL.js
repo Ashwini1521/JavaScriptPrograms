e21448a280437fafe25666e935420f5a8e338087
Ashwini1521 <ashwiniswamy1521@gmail.com>
2020-03-19 17:33:07 +0530

    [Ashwini]refactor: used get and set

--- AddressBook/AddressBookBL.js
+++ AddressBook/AddressBookBL.js
@@ -1,117 +1,108 @@
+const read = require('readline-sync');
 const fs = require('fs');
-const readLine = require('readline-sync');
 
-class person {
-    constructor(id,fname,lname,address,city,state,zip,pnumber){
+class Person {
+    constructor(name, id, address, phoneNumber, city, pincode, state) {
+        this.name = name;
         this.id = id;
-        this.fname = fname;
-        this.lname = lname;
         this.address = address;
+        this.phoneNumber = phoneNumber;
         this.city = city;
+        this.pincode = pincode;
         this.state = state;
-        this.zip = zip;
-        this.pnumber = pnumber;
     }
 }
 
-class AddressBook{
-    constructor(){
-        let bookdata = fs.readFileSync('AddressBook.json');
-        let jsonBookData = JSON.parse(bookdata);
-        this.person = [];
-        if(Array.isArray(jsonBookData.person)){
-          this.entries = { person: jsonBookData.person  };
-         }else{
-          
-            this.entries = { person: this.person  };
-         }
+class AddressbookMethods extends Person {
+    addEntry = () => {
+        const name = read.question("enter name: ");
+        const id = read.questionInt("enter id: ");
+        const address = read.question("enter address: ");
+        const phoneNumber = read.questionInt("enter phone Number: ");
+        const city = read.question("enter city: ");
+        const pincode = read.questionInt("enter pincode: ");
+        const state = read.question("enter state: ");
+        let person = new Person(name, id, address, phoneNumber, city, pincode, state);
+        this.addressbook.person.push(JSON.parse(JSON.stringify(person)));
+        fs.writeFileSync('addressbook.json', JSON.stringify(this.addressbook));
+
     }
-  
-    addEntry(){  //it will insert the data in the json file by given ID.
-        const id = readLine.questionInt('Enter the id : ');
-        const fname = readLine.question('Enter FirstName : ');
-        const lname = readLine.question('Enter LastName : ');
-        const address = readLine.question(' Enter Address : ');
-        const city = readLine.question(' Enter City : ');
-        const state = readLine.question(' Enter State : ');
-        const zip = readLine.questionInt(' Enter Pincode : ');
-        const pnumber = readLine.questionInt(' Enter Phone Number : ');
-        let p = new person(id,fname,lname,address,city,state,zip,pnumber);
-        this.entries.person.push(JSON.parse(JSON.stringify(p))); // For remove object name during push data into array 
-                                                                 // other wise it will consider object name as a key of that array.
-        fs.writeFileSync('AddressBook.json',JSON.stringify(this.entries));
-    } 
-   
-    deleteEntry(){ //it will Delete the data from the json file by given ID. 
-        let deletID = readLine.questionInt('Enter The ID for delete : ');
+    set deleteEntry(deleteId) {
         let isAvailable = true;
-        for(let i=0;i<this.entries.person.length;i++){
-            if(this.entries.person[i].id == deletID){
-                this.entries.person.splice(i,1);
-            }else{
+        for (let i = 0; i < this.addressbook.person.length; i++) {
+            if (this.addressbook.person[i].id == deleteId) {
+
+                this.addressbook.person.splice(i, 1);
+
+                isAvailable = true;
+                break;
+            } else {
                 isAvailable = false;
             }
         }
-        let deleteMessage = isAvailable?'Record Deleted Successfully.':'Record Not Founded.';
+        let deleteMessage = isAvailable ? 'Record delete Successfully' : 'Record not found';
         console.log(deleteMessage);
-        fs.writeFileSync('AddressBook.json',JSON.stringify(this.entries));
+        fs.writeFileSync('addressbook.json', JSON.stringify(this.addressbook));
     }
-  
-    editEntry(){ //it will edit the data in the json file by given ID. 
-        let editID = readLine.questionInt('Enter The Id for EDIT : ');
+    editentry = () => {
+        let editId = require.questionInt("enter id for the edit");
         let isAvailable = false;
-        for(let i=0;i<this.entries.person.length;i++){
-            if(this.entries.person[i].id === editID){
-                let editAddress = readLine.question('Enter Address For Edit : ');
-                let editCity = readLine.question('Enter City For Edit : ');
-                let editPincode = readLine.questionInt('Enter Pincode For Edit : ');
-                let editPhone = readLine.questionInt('Enter Phone Number For Edit : ');
-                this.entries.person[i].address = editAddress;
-                this.entries.person[i].city = editCity;
-                this.entries.person[i].zip = editPincode;
-                this.entries.person[i].pnumber = editPhone;
+        for (let i = 0; i < this.addressbook.person.length; i++) {
+            if (this.addressbook.person[i].id == editId) {
+                let editaddress = read.question("enter address for edit: ");
+                let editcity = read.question("enter city for edit: ");
+                let editpincode = read.questionInt("enter pincode for edit: ");
+                let editphoneNumber = read.questionInt("enter phone number for edit: ");
+                this.addressbook.person[i].address = editaddress;
+                this.addressbook.person[i].city = editcity;
+                this.addressbook.person[i].pincode = editpincode;
+                this.addressbook.person[i].phoneNumber = editphoneNumber;
                 isAvailable = true;
             }
+            let editMessage = isAvailable ? 'Record edited Successfully' : 'Record not found';
+            console.log(editMessage);
+            fs.writeFileSync('addressbook.json', JSON.stringify(this.addressbook));
         }
-        let editMessage = isAvailable?'Record Edited Successfully.':'Record Not Founded for given id.';
-        console.log(editMessage);
-        fs.writeFileSync('AddressBook.json',JSON.stringify(this.entries));
+
     }
-   
-    sortByName(){ //it will sort the data by name and update JSON file. 
-        for(let i=0;i<this.entries.person.length;i++){
-            for(let j=0;j<this.entries.person.length - 1;j++){
-                if(this.entries.person[j].fname > this.entries.person[j+1].fname){
-                    let temp = this.entries.person[j+1];
-                    this.entries.person[j+1] = this.entries.person[j];
-                    this.entries.person[j] = temp;
+    get printEntries() {
+        console.log(this.addressbook.person);
+    }
+    SortByName = () => {
+        for (let i = 0; i < this.addressbook.person.length; i++) {
+            for (let j = 0; j < this.addressbook.person.length - 1; j++) {
+                if (this.addressbook.person[j].name > this.addressbook.person[j + 1].name) {
+                    let temp = this.addressbook.person[j + 1];
+                    this.addressbook.person[j + 1] = this.addressbook.person[j];
+                    this.addressbook.person[j] = temp;
                 }
             }
         }
-        // console.log(this.entries.person);
-        console.log('Data sorted Successfully BY Name.');
-        fs.writeFileSync('AddressBook.json',JSON.stringify(this.entries));
+        console.log("Data Sorted Successfully by name");
+        fs.writeFileSync('addressbook.json', JSON.stringify(this.addressbook));
     }
-    
-    sortByZip(){  //it will sort the data by zip and update JSON file. 
-        for(let i=0;i<this.entries.person.length;i++){
-            for(let j=0;j<this.entries.person.length - 1;j++){
-                if(this.entries.person[j].zip > this.entries.person[j+1].zip){
-                    let temp = this.entries.person[j+1];
-                    this.entries.person[j+1] = this.entries.person[j];
-                    this.entries.person[j] = temp;
+    SortBypincode = () => {
+        for (let i = 0; i < this.addressbook.person.length; i++) {
+            for (let j = 0; j < this.addressbook.person.length - 1; j++) {
+                if (this.addressbook.person[j].pincode > this.addressbook.person[j + 1].pincode) {
+                    let temp = this.addressbook.person[j + 1];
+                    this.addressbook.person[j + 1] = this.addressbook.person[j];
+                    this.addressbook.person[j] = temp;
                 }
             }
         }
-        // console.log(this.entries.person);
-        console.log('Data sorted Successfully BY Zip.');
-        fs.writeFileSync('AddressBook.json',JSON.stringify(this.entries));
-    }
-  
-    printBookData(){  //it will print the all data of AddressBook JSON file.
-        console.log(this.entries.person);
+        console.log("Data Sorted Successfully by pincode");
+        fs.writeFileSync('addressbook.json', JSON.stringify(this.addressbook));
     }
 }
+class addressbook extends AddressbookMethods {
+    constructor() {
+        super()
+        let data = fs.readFileSync('addressbook.json');
+        this.addressbook = JSON.parse(data);
+    }
+}
+
 module.exports = {
-    person,AddressBook
+    addressbook
 }
