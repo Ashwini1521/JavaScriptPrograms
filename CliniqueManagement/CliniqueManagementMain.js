646d07427c3cdb343c1f6644a2386c99a9da6ed4
unknown <ashwiniswamy1521@gmail.com>
2020-03-20 13:08:26 +0530

    [Ashwini] refactor:commented on  main method

--- CliniqueManagement/CliniqueManagementMain.js
+++ CliniqueManagement/CliniqueManagementMain.js
@@ -1,11 +1,25 @@
-
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node CliniqueManagementMain.js
+	 *                      2. if nodemon installed cmd> nodemon CliniqueManagementMain.js
+	 *
+	 *  Purpose         : to create CliniqueManagement.
+	 *
+	 *  @description    : to create CliniqueManagement using oops concept...
+	 *
+	 *  @file           : CliniqueManagementMain.js
+	 *  @overview       : CliniqueManagement. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 6.13.4
+	 *  @since          : 20-03-2020
+	 ******************************************************************************/
 const CMPlib = require("./CliniqueManagementBL");
 const readline = require('readline-sync');
-try{
+try {
     let main = () => {
         let cliObj = new CMPlib.CliniqueManagement();
         let search = '';
-        do{
+        do {
             const choice = readline.questionInt(`Enter the choice to search \n******doctor****** \n doctor by \n 1. name \n 2. specialisation \n 3. Id \n ********Patient******* \n patients by \n 4. name \n 5. phone number \n 6. Id  \n 7. to exit  `);
             switch (choice) {
                 case 1: search = readline.question('enter the name of doctor you want to search ');
@@ -31,9 +45,9 @@ try{
                 default: console.log('enter valid input');
                     break;
             }
-        }while(true);
+        } while (true);
     }
     main();
-}catch(err){
+} catch (err) {
     throw err;
 }
