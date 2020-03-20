d6abe7668fedd8f31109f517b3da08295be57c0f
unknown <ashwiniswamy1521@gmail.com>
2020-03-20 13:34:59 +0530

    [Ashwini] refactor:commented on  main method

--- StockAccountManagement/stockaccountMain.js
+++ StockAccountManagement/stockaccountMain.js
@@ -1,4 +1,18 @@
-
+/******************************************************************************
+	 *  Execution       :   1. default node         cmd> node stockaccountMain.js
+	 *                      2. if nodemon installed cmd> nodemon stockaccountMain.js
+	 *
+	 *  Purpose         : to create stockaccountManagement.
+	 *
+	 *  @description    : to create stockaccount using oops concept...
+	 *
+	 *  @file           : stockaccountMain.js
+	 *  @overview       : stockaccountManagement. 
+	 *  @module         : module_name - This is optional if expeclictly its an npm or local package
+	 *  @author         : Ashwini M <ashwiniswamy1521@gmail.com>
+	 *  @version        : 6.13.4
+	 *  @since          : 20-03-2020
+	 ******************************************************************************/
 const read = require('readline-sync');
 const callfunction = require('./stockaccountBl');
 try {
