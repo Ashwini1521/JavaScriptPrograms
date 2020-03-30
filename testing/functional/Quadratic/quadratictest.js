77da6c03586fa9019999d00981e6c29185e00dd8
unknown <ashwiniswamy1521@gmail.com>
2020-03-30 11:51:17 +0530

    [Ashwini] Added test case and business logic of Quadratic program

---
+++ testing/functional/Quadratic/quadratictest.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert; //assert to determine the status of the failure
+const callfunction = require('./quadraticBl');
+
+describe('get if callfunction add.js', () => { //holds collection of test
+    
+    it("should not be null", () => {
+        assert.isNotNull(callfunction.findQuadratic(), false);
+    });
+    it("should not be character", () => {
+        assert.notEqual(callfunction.findQuadratic("abcd"), false);
+    });
+   
+});
